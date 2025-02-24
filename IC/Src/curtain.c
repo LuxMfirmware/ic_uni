88bc6e55fff903182b93e6a07d963e7c93a0d8fb
LuxMfirmware <lux_fw@outlook.com>
2025-02-24 16:30:10 +0100

    go through enabled curtains instead of all for move signal

--- IC/Src/curtain.c
+++ IC/Src/curtain.c
@@ -349,7 +349,7 @@ void Curtains_MoveSignal(const uint8_t direction)
     
     if(direction != CURTAIN_STOP)
     {
-        for(uint8_t i = 0; i < CURTAINS_SIZE; ++i)
+        for(uint8_t i = 0; i < Curtains_getCount(); ++i)
         {
             if((Curtain_hasRelays(curtains + i)) && (!(Curtain_isMoving(curtains + i) && ((curtains + i)->upDown == direction) && ((curtains + i)->upDown_old == direction)))) stop = 0;
         }
